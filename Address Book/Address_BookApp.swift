18c18d1c9ad6404a43742dfcf0c942be71e1bf60
Fawzi <fawzialrifai@icloud.com>
2022-06-11 18:30:44 +0300

    Initial Commit

---
+++ Address Book/Address_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Address_BookApp.swift
+//  Address Book
+//
+//  Created by Fawzi Rifai on 11/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Address_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Address Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Address Book
+//
+//  Created by Fawzi Rifai on 11/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
